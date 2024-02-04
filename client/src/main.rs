e2367a85695c854ddb14b2fc5ab6311546f4023d
Joe Abbate <josephabbateny@gmail.com>
2024-02-03 21:26:35 -0500

    Why did I do this?

---
+++ client/src/main.rs
@@ -0,0 +1,34 @@
+use reqwest::Client;
+use tokio::net::TcpListener;
+use tokio::io::copy_bidirectional;
+use serde_json::json;
+use std::time::Duration;
+use std::env;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        println!("Usage: {} <http api ip/port>", args[0]);
+        return Ok(());
+    }
+
+    let listener = TcpListener::bind("127.0.0.1:8080").await?;
+    println!("Waiting on 127.0.0.1:8080")
+    let mut socket = listener.accept().await?.0;
+
+    let mc_listener = TcpListener::bind("0.0.0.0:0").await?;
+
+    let port = mc_listener.local_addr().unwrap().port();
+
+    let client = Client::builder().timeout(Duration::from_secs(1)).build()?;
+    let _ = client.post(format!("http://{}/connect", args[1]))
+        .json(&json!({"port": port}))
+        .send()
+        .await;
+    let mut mc_socket = mc_listener.accept().await?.0;
+
+    copy_bidirectional(&mut socket, &mut mc_socket).await?;
+    loop{}
+    Ok(())
+}
---
+++ server/src/main.rs
@@ -0,0 +1,36 @@
+use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+use serde::Deserialize;
+use tokio::net::TcpStream;
+use tokio::io::copy_bidirectional;
+use std::env;
+
+#[derive(Deserialize)]
+struct Data {
+    port: i32,
+}
+
+async fn connect_handler(req: HttpRequest, json_data: web::Json<Data>) -> impl Responder {
+    let port = json_data.port;
+    let addr = format!("{}:{}", req.connection_info().peer_addr().unwrap(), port);
+    let mut client_stream = TcpStream::connect(addr).await.unwrap();
+    match env::var("MINECRAFT_SERVER") {
+        Ok(minecraft_ip) => {
+            let mut minecraft_stream = TcpStream::connect(minecraft_ip).await.unwrap();
+            copy_bidirectional(&mut client_stream, &mut minecraft_stream).await.unwrap();
+            "Done"
+        }
+        Err(_) => {
+            "Server is not set!"
+        }
+    }
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new().service(web::resource("/connect").route(web::post().to(connect_handler)))
+    })
+    .bind("0.0.0.0:8080")?
+    .run()
+    .await
+}
